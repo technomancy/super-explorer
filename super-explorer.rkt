8b30e52467bd193afb13006e9c16bc970858deac
Phil Hagelberg <phil@hagelb.org>
2015-02-14 17:01:33 +0700

    Implement edit mode.

--- super-explorer.rkt
+++ super-explorer.rkt
@@ -1,8 +1,9 @@
 #lang racket
 (require 2htdp/universe 2htdp/image 2htdp/planetcute)
+(require (prefix-in gui: racket/gui) racket/serialize)           ; for save
 (require "fstruct.rkt")
 
-(fstruct now (x y world-x world-y character))
+(fstruct now (x y world-x world-y world avatar tile))
 
 
 
@@ -18,67 +19,88 @@
 (define stone-tall (tile stone-block-tall false 0))
 (define wall (tile wall-block false 0))
 (define wall-tall (tile wall-block-tall false 0))
-(define wood (tile wall-block wood-block 0))
-(define door (tile door-tall-open true 0))
+(define wood (tile wood-block true 0))
+(define door (tile door-tall-closed false 0))
+(define door-open (tile door-tall-open true 0))
+
+(define tiles `#[,grass ,water ,chest ,dirt ,tree
+                        ,stone ,stone-tall ,wall ,wall-tall ,wood
+                        ,door ,door-open])
 
 
 
-(define room1 (list (list wall-tall wall-tall door wall-tall wall-tall)
-                    (list grass grass dirt grass grass)
-                    (list grass grass stone stone stone)
-                    (list grass grass grass grass grass)
-                    (list grass grass grass grass grass)))
+(define room1 (vector (vector wall-tall wall-tall door-open wall-tall wall-tall)
+                      (vector grass grass dirt grass grass)
+                      (vector grass grass stone stone stone)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)))
 
-(define room2 (list (list water grass grass grass tree)
-                    (list water water grass grass grass)
-                    (list stone stone grass grass grass)
-                    (list grass grass grass chest grass)
-                    (list stone-tall stone grass grass grass)))
+(define room2 (vector (vector water grass grass grass tree)
+                      (vector water water grass grass grass)
+                      (vector stone stone grass grass grass)
+                      (vector grass grass grass chest grass)
+                      (vector stone-tall stone grass grass grass)))
 
-(define world `#[#[,room1 ,room2]])
+(define room3 (vector (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)))
+
+(define room4 (vector (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)
+                      (vector grass grass grass grass grass)))
+
+(define init-world (vector (vector room1 room2)
+                           (vector room3 room4)))
 
 (define (room-for now)
-  (vector-ref (vector-ref world (now 'world-y)) (now 'world-x)))
+  (vector-ref (vector-ref (now 'world) (now 'world-y)) (now 'world-x)))
 
 
 
 (define (draw-row row)
-  (apply beside (map tile-image row)))
+  (apply beside (map tile-image (vector->list row))))
 
-(define (place-character row-image row character x y)
+(define (place-avatar row-image row avatar x y)
   (if (= y row)
-      (overlay/xy character (* (- 0 x) (image-width character)) 0 row-image)
+      (overlay/xy avatar (* (- 0 x) (image-width avatar)) 0 row-image)
       row-image))
 
 (define (draw now)
   (let ([x (now 'x)] [y (now 'y)]
-        [room (room-for now)] [character (now 'character)])
+        [room (room-for now)] [avatar (now 'avatar)])
     (for/fold ([scene (empty-scene 505 505)])
-              ([row (range (length room))])
-      (place-image (place-character (draw-row (list-ref room row))
-                                    row character x y)
+              ([row (range (vector-length room))])
+      (place-image (place-avatar (draw-row (vector-ref room row))
+                                 row avatar x y)
                    252 (+ 101 (* 80 row)) scene))))
 
 
 
+(define (edit-mode? now)
+  (equal? selector (now 'avatar)))
+
 (define (new-place now a-key)
-  (cond [(key=? a-key "left") (now 'x (sub1 (now 'x)))]
-        [(key=? a-key "right") (now 'x (add1 (now 'x)))]
-        [(key=? a-key "up") (now 'y (sub1 (now 'y)))]
-        [(key=? a-key "down") (now 'y (add1 (now 'y)))]
-        [else now]))
+     (cond [(key=? a-key "left") (now 'x (sub1 (now 'x)))]
+           [(key=? a-key "right") (now 'x (add1 (now 'x)))]
+           [(key=? a-key "up") (now 'y (sub1 (now 'y)))]
+           [(key=? a-key "down") (now 'y (add1 (now 'y)))]
+           [else now]))
 
 (define (off-edge? room x y)
-  (or (not (<= 0 x (sub1 (length (first room)))))
-      (not (<= 0 y (sub1 (length room))))))
+  (or (not (<= 0 x (sub1 (vector-length (vector-ref room 0)))))
+      (not (<= 0 y (sub1 (vector-length room))))))
 
 (define (allowed? room x y)
   (and (not (off-edge? room x y))
-       (tile-walkable? (list-ref (list-ref room y) x))))
+       (tile-walkable? (vector-ref (vector-ref room y) x))))
 
 (define (wrap now)
-  (let ([x-bound (sub1 (vector-length (vector-ref world 0)))]
-        [y-bound (sub1 (vector-length world))])
+  (let ([x-bound (sub1 (vector-length (vector-ref (now 'world) 0)))]
+        [y-bound (sub1 (vector-length (now 'world)))])
     (cond [(negative? (now 'world-x)) (now 'world-x x-bound)]
           [(negative? (now 'world-y)) (now 'world-y y-bound)]
           [(> (now 'world-x) x-bound) (now 'world-x 0)]
@@ -86,8 +108,8 @@
           [else now])))
 
 (define (new-room now a-key)
-  (let ([x-bound (sub1 (length (first (room-for now))))]
-        [y-bound (sub1 (length (room-for now)))])
+  (let ([x-bound (sub1 (vector-length (vector-ref (room-for now) 0)))]
+        [y-bound (sub1 (vector-length (room-for now)))])
     (cond [(key=? a-key "left") ((wrap (now 'world-x (sub1 (now 'world-x)))) 'x x-bound)]
           [(key=? a-key "right") ((wrap (now 'world-x (add1 (now 'world-x)))) 'x 0)]
           [(key=? a-key "up") ((wrap (now 'world-y (sub1 (now 'world-y)))) 'y y-bound)]
@@ -99,12 +121,52 @@
     (cond [(allowed? (room-for now) (new 'x) (new 'y)) new]
           [(off-edge? (room-for new) (new 'x) (new 'y))
            (let ([new (new-room now a-key)])
-             (if (allowed? (room-for new) (new 'x) (new 'y))
+             (if (or (edit-mode? now)
+                     (allowed? (room-for new) (new 'x) (new 'y)))
                  new
                  now))]
+          [(edit-mode? now) new]
           [else now])))
 
+
+
+(define (switch-mode now)
+  (now 'avatar (if (edit-mode? now)
+                   character-princess-girl
+                   selector)))
+
+(define (place now)
+  (when (edit-mode? now)
+    (vector-set! (vector-ref (room-for now) (now 'y)) (now 'x)
+                 (vector-ref tiles (now 'tile))))
+  now)
+
+(define (next-tile now)
+  (now 'tile (modulo (add1 (now 'tile)) (add1 (vector-length tiles)))))
+
+(define (prev-tile now)
+  (now 'tile (modulo (sub1 (now 'tile)) (vector-length tiles))))
+
+(define (save now)
+  (let ([filename (gui:put-file "Save to:")])
+    (when filename
+      (when (file-exists? filename)
+        (delete-file filename))
+      (call-with-output-file filename
+        (λ (port)
+          (write (now 'world) port))))))
+
+(define (handle-key now a-key)
+  (printf "~s~n" a-key)
+  (cond [(key=? a-key " ") (place now)] ; edit keys
+        [(key=? a-key "[") (next-tile now)]
+        [(key=? a-key "]") (prev-tile now)]
+        [(key=? a-key "0") (now 'tile 0)]
+        [(key=? a-key "\t") (switch-mode now)]
+        [(key=? a-key "s") (save now)]
+        [else (move now a-key)]))
+
 (module+ main
-  (big-bang (now 2 2 0 0 character-boy)
+  (big-bang (now 2 2 0 0 init-world character-princess-girl 0)
             (on-draw draw)
-            (on-key move)))
+            (on-key handle-key)))
