a7a3eeae75c778cf8e86bc03fe6e2e92538498e1
Phil Hagelberg <phil@hagelb.org>
2015-02-23 11:18:01 +0700

    s/world/grid/

--- super-explorer.rkt
+++ super-explorer.rkt
@@ -3,7 +3,7 @@
 (require (prefix-in gui: racket/gui) racket/serialize)           ; for save
 (require "fstruct.rkt")
 
-(fstruct now (player world tile items triggers))
+(fstruct now (player grid tile items triggers))
 
 (define (index-of l x)
   (for/or ([y l] [i (in-naturals)] #:when (equal? x y)) i))
@@ -40,8 +40,8 @@
                      (door . ,(tile door-tall-closed false 0))
                      (door-open . ,(tile door-tall-open true 0))))
 
-(define world-size-x 20)
-(define world-size-y 20)
+(define grid-size-x 20)
+(define grid-size-y 20)
 
 (define room-size-x 10)
 (define room-size-y 10)
@@ -69,16 +69,16 @@
 (define (truncate-to x y)
   (* (quotient x y) y))
 
-(define (room-for world item)
+(define (room-for grid item)
   (vector-map (lambda (row) (vector-take (vector-drop row (truncate-to (item 'x) room-size-x)) room-size-x))
-              (vector-take (vector-drop world (truncate-to (item 'y) room-size-y)) room-size-y)))
+              (vector-take (vector-drop grid (truncate-to (item 'y) room-size-y)) room-size-y)))
 
 (define (same-room? item1 item2)
   (and (= (quotient (item1 'x) room-size-x) (quotient (item2 'x) room-size-x))
        (= (quotient (item1 'y) room-size-y) (quotient (item2 'y) room-size-y))))
 
 (define (draw now)
-  (let ([room (room-for (now 'world) (now 'player))]
+  (let ([room (room-for (now 'grid) (now 'player))]
         [items (filter (curry same-room? (now 'player)) (now 'items))])
     (for/fold ([scene (empty-scene (* 101 room-size-x) (* 101 room-size-y))])
               ([row (range (vector-length room))])
@@ -94,7 +94,7 @@
   (equal? 'selector (now '(player image))))
 
 (define (out-of-bounds? item)
-  (or (> (item 'x) world-size-x) (> (item 'y) world-size-y)))
+  (or (> (item 'x) grid-size-x) (> (item 'y) grid-size-y)))
 
 (define (new-place item a-key)
   (cond [(key=? a-key "left") (dict-update item 'x sub1)]
@@ -103,8 +103,8 @@
         [(key=? a-key "down") (dict-update item 'y add1)]
         [else item]))
 
-(define (walkable? world x y)
-  (tile-walkable? (hash-ref tiles (vector-ref (vector-ref world y) x))))
+(define (walkable? grid x y)
+  (tile-walkable? (hash-ref tiles (vector-ref (vector-ref grid y) x))))
 
 (define (item-at now x y)
   (let ([is (filter (lambda (i) (and (= x (i 'x)) (= y (i 'y)))) (now 'items))])
@@ -113,7 +113,7 @@
 (define (move-push-try item now old a-key)
   (let* ([new-item (new-place item a-key)]
          [item-at-new (item-at now (new-item 'x) (new-item 'y))])
-    (cond [(and (walkable? (now 'world) (new-item 'x) (new-item 'y))
+    (cond [(and (walkable? (now 'grid) (new-item 'x) (new-item 'y))
                 (not item-at-new))
            (now 'items (cons new-item (remove item (now 'items))))]
           [(and item-at-new (item-combine item-at-new)
@@ -132,7 +132,7 @@
          [item-at-new (item-at new (new '(player x)) (new '(player y)))])
     (cond [(out-of-bounds? (new 'player)) now]
           [(edit-mode? now) new]
-          [(not (walkable? (now 'world) (new '(player x)) (new '(player y)))) now]
+          [(not (walkable? (now 'grid) (new '(player x)) (new '(player y)))) now]
           [(not item-at-new) new]
           [else (move-to-item new now a-key item-at-new)])))
 
@@ -143,7 +143,7 @@
   ((now '(player x) x) '(player y) y))
 
 (define (grassify x y now)
-  (now 'world (vector-nested-set (now 'world) (list y x) 'grass)))
+  (now 'grid (vector-nested-set (now 'grid) (list y x) 'grass)))
 
 (define (move-and-trigger now a-key)
   (let* ([moved (move now a-key)]
@@ -162,14 +162,14 @@
 
 (define (place now)
   (if (edit-mode? now)
-      (now 'world (vector-nested-set (now 'world) (map (now 'player) '(y x))
-                                     (list-ref (hash-keys tiles) (now 'tile))))
+      (now 'grid (vector-nested-set (now 'grid) (map (now 'player) '(y x))
+                                    (list-ref (hash-keys tiles) (now 'tile))))
       now))
 
 (define (place-item now item)
   (cond [(not (edit-mode? now)) now]
         [(item-at now (now '(player x)) (now '(player y))) now]
-        [(not (walkable? (now 'world) (now '(player x)) (now '(player y)))) now]
+        [(not (walkable? (now 'grid) (now '(player x)) (now '(player y)))) now]
         [else (dict-update now 'items (curry cons item))]))
 
 (define (delete-item now)
@@ -183,7 +183,7 @@
 
 (define (tile-of now)
   (now 'tile (index-of (hash-keys tiles)
-                       (vector-ref (vector-ref (now 'world)
+                       (vector-ref (vector-ref (now 'grid)
                                                (now '(player y)))
                                    (now '(player x))))))
 
@@ -227,15 +227,15 @@
                            (draw (first states)))))
               (on-key
                (lambda (states a-key)
-                        (with-handlers ([exn:fail?
-                                         (λ (e)
-                                           (printf "error ~s~n" e)
-                                           states)])
-                          (if (and (key=? a-key undo-key) (> (length states) 1))
-                              (rest states)
-                              (cons (key-handler (first states) a-key)
-                                    (take states (min max-undo-size
-                                                      (length states)))))))))))
+                 (with-handlers ([exn:fail?
+                                  (λ (e)
+                                    (printf "error ~s~n" e)
+                                    states)])
+                   (if (and (key=? a-key undo-key) (> (length states) 1))
+                       (rest states)
+                       (cons (key-handler (first states) a-key)
+                             (take states (min max-undo-size
+                                               (length states)))))))))))
 
 (module+ main
   (undoable-big-bang (call-with-input-file "world.rktd"
